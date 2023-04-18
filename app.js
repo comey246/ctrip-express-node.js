7e50c897f184435121ba791d0651b3ed49cdc53d
comey <975008777@qq.com>
2023-04-18 12:20:30 +0800

    feat:新建app入口文件

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path')
+const router = require('./router');
+const index = require('./routes/index')
+const authRouter = require('./routes/auth')
+const recordMiddleware = require('./middlewares/record')
+const useRoutes = require('./routes/userRoutes')
+const app = express();
+
+app.use(express.json())
+app.use(useRoutes)
+// app.use(authRouter);
+// app.use(router);
+app.use(index);
+app.use(recordMiddleware)
+
+exports.cache = new Map();
+
+
+//监听端口服务启动
+
+app.listen(8000,()=>{
+    console.log("服务已经启动，8000端口监听中...http://127.0.0.1:8000");
+}) 
